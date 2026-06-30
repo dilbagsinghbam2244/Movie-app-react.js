9731ef498a981e5ba96bc315c6e334c94fedd87a
Dilbag <amarjeetkaursandhu780@gmail.com>
2026-06-30 14:15:45 +0400

    add key props

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/'
 const MovieCard = ({movie}) => {
   return (
-    <Link to={`/movie/${movie.id}`} state={movie} >
+    <Link key={movie.id} to={`/movie/${movie.id}`} state={movie} >
     <div className='w-full bg-gray-900 text-white rounded-lg overflow-hidden shadow-lg hover:scale-105 transition duration-300 '>
         <img className='h-96 w-full  object-cover' src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
         <div className='p-2'>
--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -6,7 +6,7 @@ const MoviesList = ({movies}) => {
     <div className='grid grid-cols-2 md:grid-cols-5 p-4 gap-6 '>
 {
     movies.map((movie)=>(
-     movie.poster_path? <MovieCard movie={movie}/>:''
+     movie.poster_path? <MovieCard key={movie.id} movie={movie}/>:''
     ))
 }
     </div>
