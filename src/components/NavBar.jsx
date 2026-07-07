7f25a491a12d151ad00ae3cb535951017a50c119
Dilbag <amarjeetkaursandhu780@gmail.com>
2026-07-07 16:29:46 +0400

    improve sizes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({setSearch}) => {
         <h1 className='text-red-500 text-[23px] md:text-2xl font-bold'>MovieHub</h1>
         <input onChange={(e)=>{
             setSearch(e.target.value)
-        }} placeholder='Search movies...' className='bg-white outline-none rounded-md w-28 text-[14px] md:text-xl md:w-44 text-black px-3 py-1
+        }} placeholder='Search movies...' className='bg-white outline-none rounded-md w-28 text-[10px] md:text-xl md:w-44 text-black px-3 py-1
          '/>
     </div>
   )
